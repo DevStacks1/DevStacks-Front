90dacd4c88bf74575411afa4ab4e2283beb4a7d0
Vanessa <vane789vane@hotmail.com>
2021-12-16 12:24:16 -0500

    diseño login y register

--- front/src/components/Input.jsx
+++ front/src/components/Input.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const Input = ({ label, name, defaultValue, type, required }) => {
   return (
     <label htmlFor={name} className='flex flex-col my-3'>
-      <span>{label}</span>
+      <span class='font-bold text-gray-500'>{label}</span>
       <input
         required={required}
         type={type}
         name={name}
+        placeholder={name}
         className='input'
         defaultValue={defaultValue}
       />
